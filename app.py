cf79be32074bf8b1b656d16747801299c3f85a45
sparsh2002 <ui20ec62@iiitsurat.ac.in>
2023-07-30 23:07:55 +0530

    api integration done

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify , request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods=['GET'])
 def index():
-    # Pass the name to the template context
-    name = 'John'
-    return render_template('index.html', name=name)
+    if request.method=='GET':
+        try:
+            # Replace 'https://jsonplaceholder.typicode.com/posts/1' with the actual URL you want to request
+            response = requests.get('https://www.boredapi.com/api/activity?participants=1')
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Parse the JSON response content and return it
+                data = response.json()
+                return render_template('index.html' , data=data)
+            else:
+                # If the request was not successful, return an error message
+                return jsonify({'error': 'Failed to fetch data'}), 500
+
+        except requests.RequestException as e:
+            # Handle any exceptions that might occur during the request
+            return jsonify({'error': 'An error occurred during the request'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
